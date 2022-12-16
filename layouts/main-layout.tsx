5fba33a85e5b37d6b59733126a4aff254d58a3a6
Zeynep Gökçe Can <zgokcecann@hotmail.com>
2022-12-16 18:02:32 +0300

    Add layout

---
+++ layouts/main-layout.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from '../styles/Layout.module.scss'
+import Logo from '../assets/images/Logo.svg'
+import IconArrowLeft from '../assets/icons/ArrowLeft.svg'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+type MainLayoutProps = {
+	children: React.ReactNode
+}
+
+const MainLayout = ({ children }: MainLayoutProps) => {
+	const router = useRouter()
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.header}>
+				<div>
+					<Link href='/'>
+						<Image src={Logo} alt='logo' width={210} />
+					</Link>
+					<div onClick={() => router.back()}>
+						<Image src={IconArrowLeft} alt='arrow-left' width={36} />
+					</div>
+				</div>
+			</div>
+			<div className={styles.childrenWrapper}>{children}</div>
+		</div>
+	)
+}
+
+export default MainLayout
