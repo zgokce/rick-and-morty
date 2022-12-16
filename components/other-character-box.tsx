735cb381d1066eb355ba840735a1a7900c4986a4
Zeynep Gökçe Can <zgokcecann@hotmail.com>
2022-12-16 18:04:03 +0300

    Add other character box component

---
+++ components/other-character-box.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import styles from '../styles/OtherCharacterBox.module.scss'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+
+type OtherCharacterBoxProps = {
+	id: number
+	locationId: string
+	status: string
+	image: string
+	name: string
+	origin: string
+	type: string
+	gender: string
+}
+const OtherCharacterBox = ({
+	id,
+	locationId,
+	status,
+	image,
+	name,
+	origin,
+	type,
+	gender,
+}: OtherCharacterBoxProps) => {
+	//TODO: character-detail link
+	const router = useRouter()
+	return (
+		<div className={styles.container}>
+			<div
+				className={styles.imageWrapper}
+				onClick={() =>
+					router.push({
+						pathname: '/character-details',
+						query: {
+							characterId: id,
+							locationId: locationId,
+							status: status,
+						},
+					})
+				}
+			>
+				<Image src={image} alt={name} width={90} height={90} />
+			</div>
+			<div className={styles.textWrapper}>
+				<h2
+					onClick={() =>
+						router.push({
+							pathname: '/character-details',
+							query: {
+								characterId: id,
+								locationId: locationId,
+								status: status,
+							},
+						})
+					}
+				>
+					{name}
+				</h2>
+				<h3>{origin}</h3>
+				<h3>
+					{type} - {gender}
+				</h3>
+			</div>
+		</div>
+	)
+}
+
+export default OtherCharacterBox
